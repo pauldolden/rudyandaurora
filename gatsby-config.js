352b8e21989389d129ec9a931e3c09d1cad1fe71
Paul Dolden <paul@dolden.co.uk>
2021-03-20 09:29:36 +0000

    setup rss feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,8 @@ dotenv.config();
 module.exports = {
   siteMetadata: {
     title: "Rudy & Aurora",
+    description: "Rudy & Aurora Dot Com",
+    siteUrl: "https://rudyandaurora.com",
   },
   plugins: [
     `gatsby-plugin-styled-components`,
@@ -28,11 +30,62 @@ module.exports = {
         allExtensions: true, // defaults to false
       },
     },
-    // {
-    //   resolve: `gatsby-source-instagram`,
-    //   options: {
-    //     username: `29426607772`,
-    //   },
-    // },
+    {
+      resolve: "gatsby-plugin-feed",
+      options: {
+        query: `{
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+      },
+      feeds: [
+        {
+          serialize: ({ query: { site, allPrismicPost } }) => {
+            return allPrismicPost.edges.map((edge) => {
+              return Object.assign({}, edge.node.data, {
+                description: edge.node.data.short_content,
+                date: edge.node.first_publication_date,
+                url: site.siteMetadata.siteUrl + edge.node.uid,
+                guid: site.siteMetadata.siteUrl + edge.node.uid,
+              });
+            });
+          },
+          query: `
+          {
+            allPrismicPost {
+              edges {
+                node {
+                  id
+                  uid
+                  data {
+                    short_content {
+                      text
+                    }
+                    content {
+                      html
+                    }
+                    title {
+                      text
+                    }
+                  }
+                  first_publication_date(fromNow: true)
+                }
+              }
+            }
+          }
+          
+          `,
+          output: "/feed.xml",
+          title: "RSS feed",
+        },
+      ],
+    },
   ],
 };
